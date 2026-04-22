3def3596863e360ee9eacf14ddcef73af0b3465a
monxvll <stedy3348112688@gmail.com>
2026-04-22 11:08:04 -0500

    feat(UI): create basic angular´s structure

---
+++ src/app/clothes/clothes-module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ClothesRoutingModule } from './clothes-routing-module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, ClothesRoutingModule],
+})
+export class ClothesModule {}
